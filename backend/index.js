053ae13e42d0a6c9be45cba998ffc7404c9db592
unknown <sourabh.rajput.22082001@gmail.com>
2023-10-10 18:54:48 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const cors = require("cors")
 const { sequelize } = require("./config/db");
 const { userRouter } = require("./routes/user")
 require("dotenv").config();
 
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/user",userRouter);
 
